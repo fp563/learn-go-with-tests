b48ad4707c6dc426c57a74d82fbb6b5bf4211b5a
Yuriko_Matsushita <matsu.yuri@gmail.com>
2023-03-26 10:53:11 +0900

    [add] フランス語、スペイン語に対応

--- hello.go
+++ hello.go
@@ -3,14 +3,32 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const spanish = "Spanish"
+const french = "French"
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	prefix := greetingPrefix(language)
+	return fmt.Sprintf("%s%s", prefix, name)
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }
