5cc49d6ac3372cd8544586a0c5b72dd78f969621
Yuriko_Matsushita <matsu.yuri@gmail.com>
2023-03-26 23:53:18 +0900

    [fix] typo

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -19,10 +19,9 @@ func TestSum(t *testing.T) {
 	})
 }
 
-func TestSumALl(t *testing.T) {
+func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
-	// want := "bob"
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
