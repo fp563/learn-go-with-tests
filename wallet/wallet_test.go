9d5cb46ae18d5da62619c3a0232b2e65a20e6b82
Yuriko_Matsushita <matsu.yuri@gmail.com>
2023-03-27 14:37:48 +0900

    [refactor] testのリファクタリング

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -6,18 +6,20 @@ import (
 
 func TestWallet(t *testing.T) {
 
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
 		wallet.Deposit(10)
 
-		got := wallet.Balance()
-
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
@@ -25,13 +27,7 @@ func TestWallet(t *testing.T) {
 
 		wallet.Withdraw(10)
 
-		got := wallet.Balance()
-
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 }
