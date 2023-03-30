89ae2c8ad246a99b1d03bd636d9d42126e52334b
Yuriko_Matsushita <matsu.yuri@gmail.com>
2023-03-30 13:21:32 +0900

    [fix] testの引数型指定

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -9,7 +9,7 @@ func TestWallet(t *testing.T) {
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
-		wallet.Deposit(10)
+		wallet.Deposit(Bitcoin(10))
 
 		assertBalance(t, wallet, Bitcoin(10))
 	})
@@ -17,7 +17,7 @@ func TestWallet(t *testing.T) {
 	t.Run("Withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 
-		err := wallet.Withdraw(10)
+		err := wallet.Withdraw(Bitcoin(10))
 		assertNoError(t, err)
 
 		assertBalance(t, wallet, Bitcoin(10))
